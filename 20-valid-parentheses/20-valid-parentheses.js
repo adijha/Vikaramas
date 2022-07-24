036ac0ec12ce4bffc1e1b0fd8c17464145f7dc62
Aditya Kumar Jha <mail@adijha.com>
2022-07-25 01:45:38 +0530

    Time: 99 ms (38.35%), Space: 42.2 MB (58.63%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -6,13 +6,13 @@ var isValid = function(s) {
   const stack=[]
   
   for(let c of s){
-  const lastElem=stack[stack.length-1]
-      
+      const lastElem=stack[stack.length-1]
       const parentheses = c ===')' && lastElem === '('
       const braces = c ==='}' && lastElem === '{'
       const bracket = c ===']' && lastElem === '['
       if(parentheses || braces || bracket) stack.pop()
-      else stack.push(c)
+      else stack.push(c)   
   }
+    
   return !stack.length
 };
